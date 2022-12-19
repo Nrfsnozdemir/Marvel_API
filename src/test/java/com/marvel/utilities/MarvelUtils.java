4f8bfaba78136f91a6bfd860ef99281b05d573d1
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-12-19 18:18:48 +0000

    MarvelUtils.java

---
+++ src/test/java/com/marvel/utilities/MarvelUtils.java
@@ -0,0 +1,22 @@
+package com.marvel.utilities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class MarvelUtils {
+
+    private static String ts;
+    private static String apiKey;
+    private static String hash;
+
+    public static String authentication() {
+        ts = ConfigurationReader.getProperty("ts");
+        apiKey = ConfigurationReader.getProperty("apiKey");
+        hash = ConfigurationReader.getProperty("hash");
+        String authentication = "ts=" + ts + "&apikey=" + apiKey + "&hash=" + hash;
+        return authentication;
+    }
+}

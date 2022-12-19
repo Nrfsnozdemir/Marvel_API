597e9b0276365eb89b91bf47065357682127e0cc
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-12-19 18:18:35 +0000

    MarvelTestBase.java

--- src/test/java/com/marvel/utilities/MarvelTestBase.java
+++ src/test/java/com/marvel/utilities/MarvelTestBase.java
@@ -1,4 +1,13 @@
 package com.marvel.utilities;
 
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.baseURI;
+
 public abstract class MarvelTestBase {
+
+    @BeforeAll
+    public static void init(){
+        baseURI = ConfigurationReader.getProperty("marvelBaseURI");
+    }
 }

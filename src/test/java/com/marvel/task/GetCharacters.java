ac9d3cd17f415e6ab475ae65cb3bb4c129bac956
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-12-19 18:18:18 +0000

    GetCharacters.java

--- src/test/java/com/marvel/task/GetCharacters.java
+++ src/test/java/com/marvel/task/GetCharacters.java
@@ -1,4 +1,25 @@
 package com.marvel.task;
 
-public class GetCharacters {
+import com.marvel.utilities.MarvelTestBase;
+import com.marvel.utilities.MarvelUtils;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class GetCharacters extends MarvelTestBase {
+
+    @Test
+    public void test(){
+        Response response = given().accept(ContentType.JSON)
+                .when()
+                .get("/characters?" + MarvelUtils.authentication());
+
+        response.prettyPrint();
+
+
+    }
 }

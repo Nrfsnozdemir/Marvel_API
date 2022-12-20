bbc947871d5f2afc1672c93fd17ee2efd5929b60
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-12-20 23:01:14 +0000

    GetCharacters.java

--- src/test/java/com/marvel/task/GetCharacters.java
+++ src/test/java/com/marvel/task/GetCharacters.java
@@ -4,21 +4,33 @@ import com.marvel.utilities.MarvelTestBase;
 import com.marvel.utilities.MarvelUtils;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 
 public class GetCharacters extends MarvelTestBase {
 
     @Test
-    public void test(){
+    public void test() {
         Response response = given().accept(ContentType.JSON)
                 .when()
-                .get("/characters?" + MarvelUtils.authentication());
+                .get("/characters" + MarvelUtils.authentication());
 
-        response.prettyPrint();
+        //Map<String, Object> jsonMap = response.body().as(Map.class);
+        List<String> expectedProperties = Arrays.asList("id", "name", "description", "modified", "resourceURI",
+                "thumbnail", "comics", "stories", "events", "urls");
+        List<Map<String, Object>> path = response.path("data.results");
+        for (Map<String, Object> each : path) {
+            for (int i = 0; i < expectedProperties.size(); i++) {
+                assertThat(expectedProperties.get(i), each.containsKey(i));
+            }
+
+        }
 
 
     }

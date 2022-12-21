657e498697eac401d51c6438996fc972d2d4c9d7
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-12-21 11:28:43 +0000

    GetCharacters.java

--- src/test/java/com/marvel/task/GetCharacters.java
+++ src/test/java/com/marvel/task/GetCharacters.java
@@ -3,35 +3,37 @@ package com.marvel.task;
 import com.marvel.utilities.MarvelTestBase;
 import com.marvel.utilities.MarvelUtils;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GetCharacters extends MarvelTestBase {
 
+    Response response;
+    List<String> expectedProperties;
+    List<Map<String, Object>> actualProperties;
+
+    @DisplayName("Every possible character record has all JSON properties as expected")
     @Test
-    public void test() {
-        Response response = given().accept(ContentType.JSON)
+    public void test1() {
+        response = given().accept(ContentType.JSON)
                 .when()
                 .get("/characters" + MarvelUtils.authentication());
 
-        //Map<String, Object> jsonMap = response.body().as(Map.class);
-        List<String> expectedProperties = Arrays.asList("id", "name", "description", "modified", "resourceURI",
-                "thumbnail", "comics", "stories", "events", "urls");
-        List<Map<String, Object>> path = response.path("data.results");
-        for (Map<String, Object> each : path) {
-            for (int i = 0; i < expectedProperties.size(); i++) {
-                assertThat(expectedProperties.get(i), each.containsKey(i));
-            }
-
+        expectedProperties = Arrays.asList("id", "name", "description", "modified", "thumbnail", "resourceURI",
+                "comics", "stories", "events", "urls");
+        JsonPath jsonPath = response.jsonPath();
+        actualProperties = jsonPath.getList("data.results");
+        for (Map<String, Object> each : actualProperties) {
+            assertTrue(each.keySet().containsAll(expectedProperties));
         }
-
-
     }
 }

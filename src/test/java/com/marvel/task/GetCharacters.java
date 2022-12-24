85734eb859e7e11b5ea799a67da9865cf4cfd462
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-12-24 11:26:40 +0000

    GetCharacters.java

--- src/test/java/com/marvel/task/GetCharacters.java
+++ src/test/java/com/marvel/task/GetCharacters.java
@@ -13,6 +13,8 @@ import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GetCharacters extends MarvelTestBase {
@@ -20,6 +22,8 @@ public class GetCharacters extends MarvelTestBase {
     Response response;
     List<String> expectedProperties;
     List<Map<String, Object>> actualProperties;
+    int expectedValue;
+    int actualValue;
 
     @DisplayName("Every possible character record has all JSON properties as expected")
     @Test
@@ -35,5 +39,19 @@ public class GetCharacters extends MarvelTestBase {
         for (Map<String, Object> each : actualProperties) {
             assertTrue(each.keySet().containsAll(expectedProperties));
         }
+    }
+
+    @Test
+    public void test2() {
+        response = given().accept(ContentType.JSON)
+                .and().pathParam("characterId", 1010699)
+                .when()
+                .get("/characters/{characterId}" + MarvelUtils.authentication());
+
+        JsonPath jsonPath = response.jsonPath();
+        expectedValue = 14;
+        actualValue = jsonPath.getInt("data.results[0].comics.available");
+
+        assertThat(actualValue, is(expectedValue));
     }
 }
